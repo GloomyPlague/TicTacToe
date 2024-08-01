fb545077861f7e34179697a3e08b1455a124d01d
Admiral <52447692+GloomyPlague@users.noreply.github.com>
2024-08-01 11:23:51 +0800

    Add files via upload

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.aiScorelbl = new System.Windows.Forms.Label();
+            this.playerScorelbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(97, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 75);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(178, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 75);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 102);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 75);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(97, 102);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 75);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(178, 102);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 75);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(16, 183);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 75);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(97, 183);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 75);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(178, 183);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 75);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(94, 270);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(78, 13);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "Player X\'s Turn";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(107, 297);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(50, 32);
+            this.btnRestart.TabIndex = 10;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // aiScorelbl
+            // 
+            this.aiScorelbl.AutoSize = true;
+            this.aiScorelbl.Location = new System.Drawing.Point(288, 144);
+            this.aiScorelbl.Name = "aiScorelbl";
+            this.aiScorelbl.Size = new System.Drawing.Size(23, 13);
+            this.aiScorelbl.TabIndex = 11;
+            this.aiScorelbl.Text = "AI: ";
+            // 
+            // playerScorelbl
+            // 
+            this.playerScorelbl.AutoSize = true;
+            this.playerScorelbl.Location = new System.Drawing.Point(269, 122);
+            this.playerScorelbl.Name = "playerScorelbl";
+            this.playerScorelbl.Size = new System.Drawing.Size(42, 13);
+            this.playerScorelbl.TabIndex = 12;
+            this.playerScorelbl.Text = "Player: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 350);
+            this.Controls.Add(this.playerScorelbl);
+            this.Controls.Add(this.aiScorelbl);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Label aiScorelbl;
+        private System.Windows.Forms.Label playerScorelbl;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public static int playerScore = 0;
+        public static int aiScore = 0;
+
+        private char[] board = new char[9];
+        private char currentPlayer;
+        private bool gameOver;
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            gameOver = false;
+            lblStatus.Text = "";
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = ' ';
+                Button button = (Button)this.Controls.Find($"button{i + 1}", true).FirstOrDefault();
+                button.Text = "";
+                button.Enabled = true;
+                button.Click += new EventHandler(Button_Click);
+            }
+
+            if (random.Next(2) == 0)
+            {
+                currentPlayer = 'X';
+                lblStatus.Text = "Player X's turn";
+            }
+            else
+            {
+                currentPlayer = 'O';
+                lblStatus.Text = "AI's turn";
+                this.Refresh();  // Refresh the UI to show the status message
+                AITurn();
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (gameOver) return;
+
+            Button button = (Button)sender;
+            int index = int.Parse(button.Name.Replace("button", "")) - 1;
+
+            if (board[index] == ' ')
+            {
+                board[index] = currentPlayer;
+                button.Text = currentPlayer.ToString();
+                button.Enabled = false;
+
+                if (CheckWin())
+                {
+                    playerScore++;
+                    playerScorelbl.Text = "Player: " + playerScore;
+                    lblStatus.Text = $"Player {currentPlayer} wins!";
+                    gameOver = true;
+                    return;
+                }
+                if (board.All(c => c != ' '))
+                {
+                    lblStatus.Text = "It's a draw!";
+                    gameOver = true;
+                    return;
+                }
+
+                currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+                lblStatus.Text = currentPlayer == 'X' ? "Player X's turn" : "AI's turn";
+                this.Refresh();
+
+                if (currentPlayer == 'O')
+                {
+                    AITurn();
+                }
+            }
+        }
+
+        private bool CheckWin()
+        {
+            int[][] winPatterns = new int[][]
+            {
+                new int[] {0, 1, 2},
+                new int[] {3, 4, 5},
+                new int[] {6, 7, 8},
+                new int[] {0, 3, 6},
+                new int[] {1, 4, 7},
+                new int[] {2, 5, 8},
+                new int[] {0, 4, 8},
+                new int[] {2, 4, 6}
+            };
+
+            foreach (var pattern in winPatterns)
+            {
+                if (board[pattern[0]] == currentPlayer &&
+                    board[pattern[1]] == currentPlayer &&
+                    board[pattern[2]] == currentPlayer)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void AITurn()
+        {
+            int bestMove = -1;
+            int bestScore = int.MinValue;
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                if (board[i] == ' ')
+                {
+                    board[i] = 'O';
+                    int score = Minimax(board, 0, false);
+                    board[i] = ' ';
+
+                    if (score > bestScore)
+                    {
+                        bestScore = score;
+                        bestMove = i;
+                    }
+                }
+            }
+
+            if (bestMove != -1)
+            {
+                board[bestMove] = 'O';
+                Button button = (Button)this.Controls.Find($"button{bestMove + 1}", true).FirstOrDefault();
+                button.Text = "O";
+                button.Enabled = false;
+
+                if (CheckWin())
+                {
+                    aiScore++;
+                    aiScorelbl.Text = "AI: " + aiScore;
+                    lblStatus.Text = "AI wins!";
+                    gameOver = true;
+                    return;
+                }
+                if (board.All(c => c != ' '))
+                {
+                    lblStatus.Text = "It's a draw!";
+                    gameOver = true;
+                    return;
+                }
+
+                currentPlayer = 'X';
+                lblStatus.Text = "Player X's turn";
+            }
+        }
+
+        private int Minimax(char[] board, int depth, bool isMaximizing)
+        {
+            if (CheckWin())
+            {
+                return isMaximizing ? -1 : 1;
+            }
+            if (board.All(c => c != ' '))
+            {
+                return 0;
+            }
+
+            if (isMaximizing)
+            {
+                int bestScore = int.MinValue;
+
+                for (int i = 0; i < board.Length; i++)
+                {
+                    if (board[i] == ' ')
+                    {
+                        board[i] = 'O';
+                        int score = Minimax(board, depth + 1, false);
+                        board[i] = ' ';
+                        bestScore = Math.Max(score, bestScore);
+                    }
+                }
+
+                return bestScore;
+            }
+            else
+            {
+                int bestScore = int.MaxValue;
+
+                for (int i = 0; i < board.Length; i++)
+                {
+                    if (board[i] == ' ')
+                    {
+                        board[i] = 'X';
+                        int score = Minimax(board, depth + 1, true);
+                        board[i] = ' ';
+                        bestScore = Math.Min(score, bestScore);
+                    }
+                }
+
+                return bestScore;
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            InitializeGame();
+        }
+    }
+}
